5169e52b88fd0bf1d87ff023b5779e92a09b3cb5
lChap701 <lucas_chapman@stu.indianhills.edu>
2021-03-24 08:16:37 -0500

    Examples

---
+++ Examples/ASP/ASPWithDatabaseExample/default.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPWithDatabaseExample
+{
+    public partial class _default : System.Web.UI.Page
+    {
+       
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlDataSource1.InsertParameters.Add("WidgetName", TypeCode.String, "Elf Wisdom");
+            SqlDataSource1.InsertParameters.Add("WidgetPrice", TypeCode.Decimal, "27.50");
+            SqlDataSource1.InsertParameters.Add("WidgetDept", TypeCode.String, "1");
+
+            try
+            {
+                SqlDataSource1.Insert();
+            }
+            catch (Exception exc)
+            {
+                Response.Write("<script type=\"text/javascript\">alert(\"Error retrieving data" + 
+                    exc.Message + "\");</script");
+            }
+        }
+    }
+}

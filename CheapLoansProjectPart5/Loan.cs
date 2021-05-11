912c889229b2153edf2aaaad2ad945bf35c015e2
lChap701 <lucas_chapman@stu.indianhills.edu>
2021-05-11 17:56:01 -0500

    Cheap Loans Project with Selenium/NUnit Tests

---
+++ CheapLoansProjectPart5/Loan.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CheapLoansProjectPart5
+{
+    /// <summary>
+    /// Created 5/4/2021 by Lucas Chapman. 
+    /// Class for creating Loan objects for tests
+    /// </summary>
+    public class Loan
+    {
+        /// <summary>
+        /// Property for loan numbers
+        /// </summary>
+        public int LoanNum { get; set; }
+
+        /// <summary>
+        /// Property for customer names
+        /// </summary>
+        public string CustName { get; set; }
+
+        /// <summary>
+        /// Property for the amount of a loan
+        /// </summary>
+        public Nullable<decimal> LoanAmount { get; set; }
+
+        /// <summary>
+        /// Property for the annual interest rate on a loan
+        /// </summary>
+        public Nullable<decimal> AnnualIntRate { get; set; }
+
+        /// <summary>
+        /// Property for the number of payments on a loan
+        /// </summary>
+        public Nullable<int> NumPayments { get; set; }
+
+        /// <summary>
+        /// Property for the expected monthly payment of a loan
+        /// </summary>
+        public decimal MonPayment { get; set; }
+    }
+}
